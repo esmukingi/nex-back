25f266c49a0736cb7fa1e4e7bb9fcf60f666a81c
Richard <apple@Rich-2.local>
2025-04-17 01:24:54 +0200

    test

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,17 +1,50 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+
 export function initializeSocket(io) {
   const userSocketMap = {};
 
+  // Socket authentication middleware
+  io.use(async (socket, next) => {
+    try {
+      const token = socket.handshake.auth.token;
+      if (!token) {
+        return next(new Error('Authentication error: No token provided'));
+      }
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const user = await User.findById(decoded.userId).select("-password");
+      
+      if (!user) {
+        return next(new Error('Authentication error: User not found'));
+      }
+
+      socket.user = user;
+      next();
+    } catch (error) {
+      console.log("Socket authentication error:", error.message);
+      if (error.name === 'JsonWebTokenError') {
+        return next(new Error('Authentication error: Invalid token'));
+      }
+      if (error.name === 'TokenExpiredError') {
+        return next(new Error('Authentication error: Token expired'));
+      }
+      next(new Error('Authentication error'));
+    }
+  });
+
   io.on("connection", (socket) => {
-    console.log("User connected:", socket.id);
+    console.log(`User ${socket.user._id} connected with socket ${socket.id}`);
 
-    const userId = socket.handshake.query.userId;
-    if (userId) userSocketMap[userId] = socket.id;
+    // Store socket ID with user ID
+    userSocketMap[socket.user._id] = socket.id;
 
+    // Notify all clients about online users
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
     socket.on("disconnect", () => {
-      console.log("User disconnected:", socket.id);
-      delete userSocketMap[userId];
+      console.log(`User ${socket.user._id} disconnected`);
+      delete userSocketMap[socket.user._id];
       io.emit("getOnlineUsers", Object.keys(userSocketMap));
     });
   });
