64b333a184cb596ff80cbabc9155d0a0769950e4
Richard <apple@Rich-2.local>
2025-04-17 04:06:03 +0200

    done

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,15 +1,13 @@
-import express from "express";
-import { checkAuth, login, logout, signup, updateProfile } from "../controllers/auth.controller.js";
-import { protectRoute } from "../middleware/auth.middleware.js";
+import express from 'express';
+import { signup, login, logout, checkAuth, updateProfile } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/protectRoute.js';
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
-router.post("/logout", logout);
-
-router.put("/update-profile", protectRoute, updateProfile);
-
-router.get("/check", protectRoute, checkAuth);
+router.post('/signup', signup);
+router.post('/login', login);
+router.post('/logout', logout);
+router.get('/check', checkAuth);
+router.post('/updateProfile', protectRoute, updateProfile);
 
 export default router;
